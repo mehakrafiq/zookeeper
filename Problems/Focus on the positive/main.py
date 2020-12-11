ffe05e035ba4b060a2c3cf2ea0fe2756ca55211b
Mehak Rafiq <19900704+mehakrafiq@users.noreply.github.com>
2020-12-11 15:25:48 +0500

    Add files via upload

---
+++ Problems/Focus on the positive/main.py
@@ -0,0 +1,4 @@
+# Please don't modify this code
+# `a` stores an input value
+a = int(input().strip())
+print( 0 < a)
---
+++ Problems/List from string/main.py
@@ -0,0 +1,4 @@
+# the following line reads the string to work with from the input, do not modify it, please
+input_str = input()
+list_of_input = list(input_str)
+print(list_of_input)
